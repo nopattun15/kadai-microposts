978c40e16ca0daa003ef7434372ecd6718dc60fe
patuo <nopattun15@gmail.com>
2019-05-28 13:47:28 +0000

    Favorite system

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,6 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+  has_many :favorites,dependent: :destroy
+  has_many :users, through: :favorites
   validates :content, presence: true, length: {maximum: 255}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,9 @@ class User < ApplicationRecord
     has_many :reverses_of_relationships, class_name: "Relationship", foreign_key: "follow_id"
     has_many :followers, through: :reverses_of_relationships, source: :user
     
+    has_many :favorites
+    has_many :favorites_microposts, through: :favorites, source: :micropost
+    
     def follow(other_user)
         unless self == other_user
             self.relationships.find_or_create_by(follow_id: other_user.id)
@@ -29,5 +32,18 @@ class User < ApplicationRecord
     
     def feed_microposts
         Micropost.where(user_id: self.following_ids + [self.id])
+    end 
+    
+    def favorite(user_micropost)
+        self.favorites.find_or_create_by(micropost_id: user_micropost.id)
+    end
+    
+    def unfavorite(user_micropost)
+        favorite = self.favorites.find_by(micropost_id: user_micropost.id)
+        favorite.destroy if favorite
+    end 
+    
+    def favorite?(user_micropost)
+        self.favorites_microposts.include?(user_micropost)
     end 
 end
